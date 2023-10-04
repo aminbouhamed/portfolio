630c728ce75c6eb901fc7cb18b7c06e9319c36fa
amin bouhamed <amin.bouhamed@esprit.tn>
2023-10-04 11:54:31 +0100

    seventh commit

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,11 +2,8 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
 import emotion from "../../Assets/Projects/emotion.png";
 import rvm from "../../Assets/Projects/5.png";
-
-import suicide from "../../Assets/Projects/suicide.png";
 
 
 function Projects() {
@@ -30,30 +27,7 @@ function Projects() {
               title="Reverse Vending Machine"
               description="Built a Prototype of cans and bottles sorter using computer vison pretrained model with tensorflow linked with a mobile application contain a gammification system"
               ghLink="https://github.com/aminbouhamed/reverse-vending-machine-django-api"
-              
-            />
-          </Col>
-
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={leaf}
-              isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={suicide}
-              isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
+              demoLink="https://drive.google.com/file/d/1iYF6ZegnNlID7g1da0yCUDRWzoDHxynM/view?usp=sharing" 
             />
           </Col>
 
@@ -64,8 +38,8 @@ function Projects() {
               title="Face Recognition and Emotion Detection"
               description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
               Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+              ghLink="https://github.com/aminbouhamed/facial_emotion_detection"
+               
             />
           </Col>
         </Row>
